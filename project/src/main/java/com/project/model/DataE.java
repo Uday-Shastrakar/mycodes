7ed132845bd88593796ec87b2ea1fe0f0eaeb900
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-01-16 18:09:26 +0530

    Add files via upload

---
+++ project/src/main/java/com/project/model/DataE.java
@@ -0,0 +1,32 @@
+package com.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "employee")
+public class DataE {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+
+@Column(name = "first_name")
+private String firstName;
+
+@Column(name = "last_name")
+private String lastName;
+
+@Column(name= "email")
+private String email;
+
+
+
+
+}
